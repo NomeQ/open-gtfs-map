c592adad3d9dbb5e1929c5a85cf11bf61964f30d
Nome <dickerson.nomi@gmail.com>
2017-07-13 11:47:18 -0700

    Improved routing with errors

--- open-gtfs-map/routes/map.js
+++ open-gtfs-map/routes/map.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     req.checkQuery('gtfs_url', 'Valid URL Required').notEmpty().isUrl();
-    req.sanitize('gtfs_url').escape();
     req.sanitize('gtfs_url').trim();
     var errors = req.validationErrors();
     var gtfs_url = req.query.gtfs_url;
