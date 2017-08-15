6bfa721fe4e9211d9d85be98131bf1eb42cf813a
Nome Q <dickerson.nomi@gmail.com>
2017-08-14 17:20:33 -0700

    Front-end Interactivity

---
+++ open-gtfs-map/public/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('#legend a').on('mouseenter click', function() {
+		var routeClass = $(this).attr('class');
+		showHighlight(routeClass);
+	}).on('mouseout mouseleave', function() {
+		var routeClass = $(this).attr('class');
+		hideHighlight(routeClass);
+	});
+	$('#highlights g').on('mouseenter click', function() {
+		var routeClass = $(this).attr('class');
+		showHighlight(routeClass);
+	}).on('mouseout mouseleave', function() {
+		hideHighlight('focused');
+	});
+});
+
+function showHighlight(routeClass) {
+	routeClass = '.' + routeClass;
+	$('#highlights').find(routeClass).addClass("focused");
+	$('#outlines').find(routeClass).addClass("focused");
+}
+
+function hideHighlight(routeClass) {
+	routeClass = '.' + routeClass;
+	$('#highlights').find(routeClass).removeClass("focused");
+	$('#outlines').find(routeClass).removeClass("focused");
+}
